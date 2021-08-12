619c10806efd2cc6ef7c67e6109be2be15d2381c
Tarasenko Yuriy <y.tarasenko@NB0520.local>
2021-08-12 20:03:31 +0300

    Create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import '@bizone/ui-bundle/esm/bundle.css';
 import React from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+import { ThemeProvider, defaultLight, defaultDark } from '@combinezone/theme';
 
 import { AuthHandler } from 'components/AuthHandler';
 import { Routes } from 'routes';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const App: React.FC = () => {
   return (
-    <AuthHandler>
-      <Routes />
-    </AuthHandler>
+    <QueryClientProvider client={queryClient}>
+      <AuthHandler>
+        <ThemeProvider theme={defaultLight} darkTheme={defaultDark}>
+          <Routes />
+        </ThemeProvider>
+      </AuthHandler>
+    </QueryClientProvider>
   );
 };
 
